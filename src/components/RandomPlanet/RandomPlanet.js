f3a90c3e68d854a7bb134f37c9b91966270bd61c
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-06-21 01:25:34 +0300

    Add change of random planet at intervals

--- src/components/RandomPlanet/RandomPlanet.js
+++ src/components/RandomPlanet/RandomPlanet.js
@@ -15,9 +15,13 @@ export default class RandomPlanet extends Component {
     loading: true,
   };
 
-  constructor() {
-    super();
+  componentDidMount() {
     this.updatePlanet();
+    this.interval = setInterval(this.updatePlanet, 10000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   onPlanetLoaded = (planet) => {
