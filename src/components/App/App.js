d9c38fccf0f32c2644289590fa457f8b442de35f
AlenaGoncharova <alen.gon4arowa@gmail.com>
2020-07-01 23:30:30 +0300

    Add handling of nonexistent addresses

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,7 +8,7 @@ import DummySwapiService from '../../services/dummySwapiService';
 import { SwapiServiceProvider } from '../SwapiServiceContext';
 import { PeoplePage, PlanetsPage, StarshipsPage, LoginPage, SecretPage } from '../pages';
 
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import { StarshipDetails } from '../sw-components';
@@ -44,29 +44,33 @@ export default class App extends Component {
               <Header onServiceChange={this.onServiceChange}/>
               <RandomPlanet />
 
-              <Route path="/" render={() => <h2>Welcome to StarDB!</h2>} exact />
-              <Route path="/people/:id?" component={PeoplePage} />
-              <Route path="/planets" component={PlanetsPage} />
-              <Route path="/starships" exact component={StarshipsPage} />
-              <Route
-                path="/starships/:id"
-                render={({ match }) => {
-                  const { id } = match.params;
-                  return <StarshipDetails itemId={id} />
-                }} 
-              />
-              <Route
-                path="/login"
-                render={() => (
-                  <LoginPage isLoggedIn={isLoggedIn} onLogin={this.onLogin} />
-                )}
-              />
-              <Route
-                path="/secret"
-                render={() => (
-                  <SecretPage isLoggedIn={isLoggedIn}/>
-                )}
-              />
+              <Switch>
+                <Route path="/" render={() => <h2>Welcome to StarDB!</h2>} exact />
+                <Route path="/people/:id?" component={PeoplePage} />
+                <Route path="/planets" component={PlanetsPage} />
+                <Route path="/starships" exact component={StarshipsPage} />
+                <Route
+                  path="/starships/:id"
+                  render={({ match }) => {
+                    const { id } = match.params;
+                    return <StarshipDetails itemId={id} />
+                  }} 
+                />
+                <Route
+                  path="/login"
+                  render={() => (
+                    <LoginPage isLoggedIn={isLoggedIn} onLogin={this.onLogin} />
+                  )}
+                />
+                <Route
+                  path="/secret"
+                  render={() => (
+                    <SecretPage isLoggedIn={isLoggedIn}/>
+                  )}
+                />
+
+                <Route render={() => <h2>Page not found</h2>}/>
+              </Switch>
 
             </div>
           </Router>
